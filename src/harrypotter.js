50140980ca38d5eec4836d8a4c613e31e2ffbf97
Priscilla-22 <priscillamw123@gmail.com>
2024-01-16 20:52:56 +0300

    fix: handle undefined values for other properties in cast details Ensure other properties are not displayed if they are undefined or null.

--- src/harrypotter.js
+++ src/harrypotter.js
@@ -29,19 +29,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const castName = document.createElement('h3');
     castName.classList.add('cast-name');
-    castName.textContent = `${cast.name}`;
+    castName.textContent = cast.name || '';
 
-    const altName = document.createElement('p');
-    altName.classList.add('alt-name');
+  const altName = document.createElement('p');
+  altName.classList.add('alt-name');
+  if (
+    cast.alternate_names !== undefined &&
+    cast.alternate_names !== null &&
+    cast.alternate_names.length > 0
+  ) {
     altName.innerHTML = `<strong>AKA:</strong> ${cast.alternate_names}`;
+  }
 
     const actor = document.createElement('p');
     actor.classList.add('actor');
-    actor.innerHTML = `<strong>Actor:</strong> ${cast.actor}`;
+    actor.innerHTML = `<strong>Actor:</strong> ${cast.actor || ''}`;
 
     const personality = document.createElement('p');
     personality.classList.add('personality');
-    personality.innerHTML = `<strong>Personality: </strong>${cast.species} <span>|</span> ${cast.gender} <hr>`;
+    personality.innerHTML = `<strong>Personality: </strong>${
+      cast.species || ''
+    } <span>|</span> ${cast.gender || ''} <hr>`;
 
     castContainer.appendChild(castDetailsContainer);
     castDetailsContainer.append(
@@ -70,42 +78,72 @@ document.addEventListener('DOMContentLoaded', () => {
     return moreDetailsContainer;
   }
 
-function createMoreDetailsContainer(cast) {
-  const moreDetailsContainer = document.createElement('details');
-  moreDetailsContainer.id = 'more-details-container';
+  function createMoreDetailsContainer(cast) {
+    const moreDetailsContainer = document.createElement('details');
+    moreDetailsContainer.id = 'more-details-container';
 
-  const moreDetailsBtn = document.createElement('summary');
-  moreDetailsBtn.innerText = 'More Details ';
-  moreDetailsBtn.classList.add('toggle-details-btn');
+    const moreDetailsBtn = document.createElement('summary');
+    moreDetailsBtn.innerText = 'More Details ';
+    moreDetailsBtn.classList.add('toggle-details-btn');
 
-  moreDetailsContainer.appendChild(moreDetailsBtn);
-  moreDetailsContainer.appendChild(createMoreDetailsContent(cast));
+    moreDetailsContainer.appendChild(moreDetailsBtn);
+    moreDetailsContainer.appendChild(createMoreDetailsContent(cast));
 
-  return moreDetailsContainer;
-}
+    return moreDetailsContainer;
+  }
 
-function createMoreDetailsContent(cast) {
-  const moreDetailsContent = document.createElement('div');
+  function createMoreDetailsContent(cast) {
+    const moreDetailsContent = document.createElement('div');
 
-  moreDetailsContent.innerHTML = `
-    <p><strong> Wand:</strong> ${cast.wand.wood} <span>|</span> ${
-    cast.wand.core
-  }<span>|</span> ${cast.wand.length}</p>   
-    <p><strong>House:</strong> ${cast.house}</p>
-    <p><strong>Date of Birth:</strong> ${cast.dateOfBirth}</p>
-    <p><strong>Year of Birth:</strong> ${cast.yearOfBirth}</p>
-    <p><strong>Wizard:</strong> ${cast.wizard ? 'Yes' : 'No'}</p>
-    <p><strong>Ancestry:</strong> ${cast.ancestry}</p>
-    <p><strong>Eye color:</strong> ${cast.eyeColour}</p>
-    <p><strong>Hair Color:</strong> ${cast.hairColour}</p>
-    <p><strong>Patronus:</strong> ${cast.patronus}</p>
-    <p><strong>Hogwart's Student:</strong> ${cast.hogwartsStudent}</p>
-    <p><strong>Hogwart's Staff:</strong> ${cast.hogwartsStaff}</p>
-    <p><strong>Alive:</strong> ${cast.alive}</p>
+    moreDetailsContent.innerHTML = `
+    ${
+      cast.wand && cast.wand.wood
+        ? `<p><strong> Wand:</strong> ${cast.wand.wood} <span>|</span> ${cast.wand.core} <span>|</span> ${cast.wand.length} </p>`
+        : ''
+    }
+    ${cast.house ? `<p><strong>House:</strong> ${cast.house}</p>` : ''}
+    ${
+      cast.dateOfBirth
+        ? `<p><strong>Date of Birth:</strong> ${cast.dateOfBirth}</p>`
+        : ''
+    }
+    ${
+      cast.yearOfBirth
+        ? `<p><strong>Year of Birth:</strong> ${cast.yearOfBirth}</p>`
+        : ''
+    }
+    ${
+      cast.wizard
+        ? `<p><strong>Wizard:</strong> ${cast.wizard ? 'Yes' : 'No'}</p>`
+        : ''
+    }
+    ${cast.ancestry ? `<p><strong>Ancestry:</strong> ${cast.ancestry}</p>` : ''}
+    ${
+      cast.eyeColour
+        ? `<p><strong>Eye color:</strong> ${cast.eyeColour}</p>`
+        : ''
+    }
+    ${
+      cast.hairColour
+        ? `<p><strong>Hair Color:</strong> ${cast.hairColour}</p>`
+        : ''
+    }
+    ${cast.patronus ? `<p><strong>Patronus:</strong> ${cast.patronus}</p>` : ''}
+    ${
+      cast.hogwartsStudent
+        ? `<p><strong>Hogwart's Student:</strong> ${cast.hogwartsStudent}</p>`
+        : ''
+    }
+    ${
+      cast.hogwartsStaff
+        ? `<p><strong>Hogwart's Staff:</strong> ${cast.hogwartsStaff}</p>`
+        : ''
+    }
+    ${cast.alive ? `<p><strong>Alive:</strong> ${cast.alive}</p>` : ''}
   `;
 
-  return moreDetailsContent;
-}
+    return moreDetailsContent;
+  }
 
   function goBack() {
     currentCastId = Math.max(1, currentCastId - 1);
